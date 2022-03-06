1a83b83a33af142e40b610b575413bfce7f5f40d
raulbellosom <raul.belloso.m@gmail.com>
2022-03-05 22:23:33 -0600

    cambio nombre proyecto

--- resources/views/layouts/light.blade.php
+++ resources/views/layouts/light.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
-    <title>SISRAD</title>
+    <title>REPORTEC</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
